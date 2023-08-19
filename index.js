60cf4a21a40c5383c0ab245e68d4ef9a6bbe9905
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-08-19 21:33:45 +0100

    practicing with the api

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 
 
 const myKey = '141dd68d'
+
+document.getElementById('src-btn').addEventListener('click', () => {
+    const inputValue = document.getElementById('title-input').value
+    fetch(`http://www.omdbapi.com/?s=${inputValue}&apikey=141dd68d`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
+
