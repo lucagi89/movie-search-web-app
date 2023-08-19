d7bdf0c1e1522cc409058588737e35a5b32ecc98
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-08-19 22:57:47 +0100

    first actual rendering

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 
-
+let moviesId = []
+let moviesArray = []
 const myKey = '141dd68d'
 
 document.getElementById('src-btn').addEventListener('click', async () => {
     const inputValue = document.getElementById('title-input').value
     const res = await fetch(`http://www.omdbapi.com/?s=${inputValue}&apikey=${myKey}`)
     const data = await res.json()
-    console.log(data)
-    const moviesHtml = data.Search.map(movie => 
-        `<h3>${movie.Title}</h3>
-        <img src='${movie.Poster}'>`)
+    data.Search.map(movie => 
+        moviesId.push(movie.imdbID))
+        getFullMovieData(moviesId)
 
-    document.getElementById('movies-list').innerHTML = moviesHtml
+    // document.getElementById('movies-list').innerHTML = moviesHtml
     })
 
+function getFullMovieData(moviesId){
+    moviesId.map(async (movie) => {
+        const res = await fetch(`http://www.omdbapi.com/?i=${movie}&apikey=${myKey}`)
+        const data = await res.json()
+        moviesArray.push(data)
+    })
+    // console.log(moviesArray)
+    render(moviesArray)
+}
+
+function render(objArray){
+    let html = ''
+    
+    objArray.map(movie => {
+        const {Title, imdbRating, Runtime, Genre, Plot, Poster} = movie
+        html += `
+        <h1>${Title}</h1>
+        <p>${imdbRating}</p><p>${Runtime}</p>
+        <h3>${Genre}</h3>
+        <p>${Plot}</p>
+        <img src='${Poster}'>
+        `
+    })
+    document.getElementById('movies-list').innerHTML = html
+}
